e6adf799a3fc388d489d3b12068622a3e9b5e390
SantanuDatta <40891517+SantanuDatta@users.noreply.github.com>
2018-09-22 20:39:02 +0600

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,10 @@
+$(function () {
+    $("body").niceScroll({
+        cursorcolor:"#d75d0e",
+        cursorwidth:"10px",
+        cursorborder:"0px",
+		cursorfixedheight: 100,
+    });
+	new WOW().init();
+        
+});
